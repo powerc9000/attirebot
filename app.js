c0c2418b589c0badddef9c88cc36757fa3304c8a
Clay Murray <clay.murray8@gmail.com>
2013-05-30 22:19:57 -0600

    Added in the base things it will need to get started still need to do DB

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var RedisStore = require('connect-redis')(express)
+var app = express();
+var MemStore = express.session.MemoryStore;
+var router = require("./router");
+var flash = require('connect-flash');
+var config = require("./config")
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.static(__dirname + "/public"));
+  app.use(express.favicon());
+  app.use(express.bodyParser());
+
+  app.use(express.cookieParser(config.sessionSecret));  
+  app.use(express.session({store: new RedisStore()}));
+  app.use(flash());
+});
+
+router.call(app);
+app.listen(config.port);
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 It's going to be rad!
 =====================
+
+If you install this repo for the first time you need to change config.sample.js to config.js
